8c70e80729e746e905b0754c6c1c8692bf24a4e4
Reinaldo Rossetti <reinaldo.rossetti@paygo.com.br>
2021-11-18 14:04:18 -0300

    removendo spring das dependencias

--- src/main/kotlin/qa/perfomance/JmeterKotlinApplication.kt
+++ src/main/kotlin/qa/perfomance/JmeterKotlinApplication.kt
@@ -1,11 +1,1 @@
 package qa.perfomance
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-
-@SpringBootApplication
-class JmeterKotlinApplication
-
-fun main(args: Array<String>) {
-	runApplication<JmeterKotlinApplication>(*args)
-}
