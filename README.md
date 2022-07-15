8db399614194299e1c020e3508fcc91983ab7060
Reinaldo Rossetti <reinaldo.rossetti@paygo.com.br>
2022-07-15 18:09:22 -0300

    melhorando os testes

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # jmeterKotlin
 Teste usando o Jmeter DSL em Kotlin
+
+Linha de comando:
+````bash
+mvn clean test -Dtest=PerformanceServeRestUsuarios -DthreadCount=50 -Diterations=1
+mvn clean test -Dtest=PerformanceServeRestUsuarios -DthreadCount=100 -Diterations=1
+mvn clean test -Dtest=PerformanceServeRestUsuarios -DthreadCount=200 -Diterations=1
+mvn clean test -Dtest=PerformanceServeRestUsuarios -DthreadCount=300 -Diterations=1
+mvn clean test -Dtest=PerformanceServeRestUsuarios -DthreadCount=400 -Diterations=1
+mvn clean test -Dtest=PerformanceServeRestUsuarios -DthreadCount=600 -Diterations=1
+mvn clean test -Dtest=PerformanceServeRestUsuarios -DthreadCount=900 -Diterations=1
+mvn clean test -Dtest=PerformanceServeRestUsuarios -DthreadCount=1000 -Diterations=1
+````
--- src/test/kotlin/qa/perfomance/tests/performance/ServeRest.kt
+++ src/test/kotlin/qa/perfomance/tests/performance/ServeRest.kt
@@ -24,6 +24,7 @@ class PerformanceServeRestUsuarios: BaseTestPerformance() {
     private var configJmeter = setUp()
     // usando o Faker para gerar dados aleatorios.
     var faker = Faker()
+    var URL = "http://192.168.18.4:3000/"
 
     /**
      * 3 threadGroup + threadCount=560 + iterations=3 ->> Igual a 5.040 requests.
@@ -37,14 +38,14 @@ class PerformanceServeRestUsuarios: BaseTestPerformance() {
     fun testPerformanceThreadGroup() {
         // create object of Path
         val currentDate = SimpleDateFormat("ddMMyyyy").format(Date())
-        val name = "//reportJmeter//$currentDate"
+        val name = "target//reportJmeter//$currentDate"
         //FileUtils.deleteDirectory(File(pathProject + name))
 
         val stats = testPlan(
             // X (configJmeter.threadCount) threads for Y (configJmeter.iterations) iterations each
-            // Sample: 10 threads\users virtuais (usuários simultâneos) que enviam 10 solicitações HTTP.
+            // Sample: 50 threads\users virtuais (usuários simultâneos) que enviam 50 solicitações HTTP.
             threadGroup(configJmeter.threadCount, configJmeter.iterations).children(
-                httpSampler("https://serverest.dev/usuarios/")
+                httpSampler("$URL/usuarios/")
                     .method(HTTPConstants.POST)
                     .post("{ \"nome\": \"\${NOME}\", \"email\": \"\${EMAIL}\", \"password\": \"\${PASS}\", \"administrador\": \"true\" }",
                         ContentType.TEXT_PLAIN)
@@ -52,19 +53,19 @@ class PerformanceServeRestUsuarios: BaseTestPerformance() {
                         jsr223PreProcessor { s -> s.vars.put("EMAIL", buildRequestBody(s.vars)) }
                     )
                     .contentType(ContentType.APPLICATION_JSON)
-                    .header("Accept", "application/json")
-                    .header("monitor", "false"),
+                    .header("Accept", "application/json"),
+                    //header("monitor", "false"),
                 // Temporizador Uniforme Aleatório que pausa a thread com um tempo aleatório com distribuição uniforme.
                 // valor minimo e valor maximo em milissegundo
-                uniformRandomTimer(500, 3000),
+                uniformRandomTimer(500, 5000),
             ),
             threadGroup(configJmeter.threadCount, configJmeter.iterations).children(
-                httpSampler("https://serverest.dev/usuarios/")
+                httpSampler("$URL/usuarios/")
                 .method(HTTPConstants.GET)
                 .contentType(ContentType.APPLICATION_JSON)
-                .header("Accept", "application/json")
-                .header("monitor", "false"),
-                uniformRandomTimer(500, 3000),
+                .header("Accept", "application/json"),
+                //.header("monitor", "false"),
+                uniformRandomTimer(500, 5000),
                 responseAssertion().containsSubstrings("quantidade"),
         ),
         // pega o log de cada request
@@ -78,57 +79,6 @@ class PerformanceServeRestUsuarios: BaseTestPerformance() {
         // dashboard em tempo real.
         // dashboardVisualizer(),
         htmlReporter(name),
-        ).run()
-        // quantidade de erros igual a zero.
-        assertThat(stats.overall().errorsCount()).isEqualTo(0)
-        // o tempo maximo de ser de 10 segundos.
-        assertThat(stats.overall().sampleTimePercentile99()).isLessThan(Duration.ofSeconds(10))
-    }
-
-    @Test
-    @Disabled
-    @Throws(IOException::class)
-    fun testPerformanceUsuarios() {
-        // create object of Path
-        val sdf = SimpleDateFormat("ddMMyyyy")
-        val currentDate = sdf.format(Date())
-        val name = "target//reportJmeter//$currentDate"
-        FileUtils.deleteDirectory(File(pathProject + name))
-
-        val stats = testPlan(
-            threadGroup("Group - cadastro de usuarios").children(
-                httpSampler("https://serverest.dev/usuarios/")
-                    .method(HTTPConstants.POST)
-                    .post("{ \"nome\": \"\${NOME}\", \"email\": \"\${EMAIL}\", \"password\": \"\${PASS}\", \"administrador\": \"true\" }",
-                        ContentType.TEXT_PLAIN)
-                    .children(
-                        jsr223PreProcessor { s -> s.vars.put("EMAIL", buildRequestBody(s.vars)) }
-                    )
-                    .contentType(ContentType.APPLICATION_JSON)
-                    .header("Accept", "application/json")
-                    .header("monitor", "false"),
-                uniformRandomTimer(500, 3000),
-            ).rampTo(configJmeter.threadCount, Duration.ofSeconds(5)).holdIterating(configJmeter.iterations),
-            threadGroup("Group - usuarios").children(
-                httpSampler("https://serverest.dev/usuarios/")
-                    .method(HTTPConstants.GET)
-                    .contentType(ContentType.APPLICATION_JSON)
-                    .header("Accept", "application/json")
-                    .header("monitor", "false"),
-                uniformRandomTimer(500, 3000),
-                responseAssertion().containsSubstrings("quantidade"),
-            ).rampTo(configJmeter.threadCount, Duration.ofSeconds(configJmeter.timeoutIteration)).holdIterating(configJmeter.iterations),
-            // pega o log de cada request
-            jtlWriter("//target//reportJmeter//logs_" + Instant.now().toString().replace(":", "-") + ".jtl"),
-
-            // pega o retorno da requisicao em arquivos, vai pegar de cada requisição
-            // responseFileSaver(Instant.now().toString().replace(":", "-") + "-response"),
-
-            // visualizando em grafico de arvore.
-            // resultsTreeVisualizer(),
-            // dashboard em tempo real.
-            dashboardVisualizer(),
-            htmlReporter(name),
         ).run()
         // quantidade de erros igual a zero.
         assertThat(stats.overall().errorsCount()).isEqualTo(0)
